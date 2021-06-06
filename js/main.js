a999ef48ae4bd7f807878e510b64b6d8880f2def
Andrei <i.andrei.abramov@gmail.com>
2021-06-06 23:09:40 +0300

    Доработаны функции getRandomPositiveInteger и getRandomPositiveFloat с учетом файлов из папки utils, полученных из master.

--- js/main.js
+++ js/main.js
@@ -17,24 +17,22 @@ const LONGITUDES = {
   max: 139.80000,
 };
 
-const getRandomInteger = (min, max) => {
-  let startValue = Math.min(min, max);
-  let endValue = Math.max(min, max);
-  if (min < 0) {
-    return null;
-  }
+const getRandomPositiveInteger = (min, max) => {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  let startValue = Math.ceil(Math.min(min, max));
+  let endValue = Math.floor(Math.max(min, max));
   startValue -= 0.5;
   endValue += 0.5;
   const randomInteger = startValue + Math.random() * (endValue - startValue);
   return Math.round(randomInteger);
 };
 
-const getRandomFloat = (min, max, decimalsNumber) => {
+const getRandomPositiveFloat = (min, max, decimalsNumber) => {
+  min = Math.abs(min);
+  max = Math.abs(max);
   const startValue = Math.min(min, max);
   const endValue = Math.max(min, max);
-  if (startValue < 0) {
-    return null;
-  }
   const randomFloat = startValue + Math.random() * (endValue - startValue);
   const result = +randomFloat.toFixed(decimalsNumber);
 
@@ -45,7 +43,7 @@ const getRandomFloat = (min, max, decimalsNumber) => {
   return result;
 };
 
-const getRandomItemsFromArr = (array) => array.filter(() => getRandomInteger(0, 1) === 1);
+const getRandomItemsFromArr = (array) => array.filter(() => getRandomPositiveInteger(0, 1) === 1);
 
 const getNewAd = (currentUser) => {
   const newAd = {
@@ -56,18 +54,18 @@ const getNewAd = (currentUser) => {
       title: 'Только лучшие апартаменты!',
       address: 'location.x, location.y',
     },
-    price: getRandomInteger(0, 10000),
-    type: HOUSING_TYPES[getRandomInteger(0, HOUSING_TYPES.length - 1)],
-    rooms: getRandomInteger(1, 10),
-    guests: getRandomInteger(1, 10),
-    checkin: TIMES[getRandomInteger(0, TIMES.length - 1)],
-    checkout: TIMES[getRandomInteger(0, TIMES.length - 1)],
+    price: getRandomPositiveInteger(0, 10000),
+    type: HOUSING_TYPES[getRandomPositiveInteger(0, HOUSING_TYPES.length - 1)],
+    rooms: getRandomPositiveInteger(1, 10),
+    guests: getRandomPositiveInteger(1, 10),
+    checkin: TIMES[getRandomPositiveInteger(0, TIMES.length - 1)],
+    checkout: TIMES[getRandomPositiveInteger(0, TIMES.length - 1)],
     features: getRandomItemsFromArr(FEATURES_LIST),
     description: 'Бери не пожалеешь! Век усов не видать!',
     photos: getRandomItemsFromArr(PHOTOS_LIST),
     location: {
-      lat: getRandomFloat(LATITUDES.min, LATITUDES.max, 5),
-      lng: getRandomFloat(LONGITUDES.min, LONGITUDES.max, 5),
+      lat: getRandomPositiveFloat(LATITUDES.min, LATITUDES.max, 5),
+      lng: getRandomPositiveFloat(LONGITUDES.min, LONGITUDES.max, 5),
     },
   };
   return newAd;
@@ -77,7 +75,7 @@ const generateAdsArray = (numberOfAds) => {
   const adsArray = [];
   const copyOfUsers = [...USERS];
   for (let index = 0; index < numberOfAds; index++) {
-    let currentUser = copyOfUsers.splice(getRandomInteger(0, copyOfUsers.length - 1), 1);
+    let currentUser = copyOfUsers.splice(getRandomPositiveInteger(0, copyOfUsers.length - 1), 1);
 
     currentUser = (currentUser.length > 0) ? currentUser : 'unknownRaccoon';
     const adItem = getNewAd(currentUser);
