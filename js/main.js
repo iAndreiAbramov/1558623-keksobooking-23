91ca5fa8788e31c894b0bbe6cc14c8db46c09435
Andrei <i.andrei.abramov@gmail.com>
2021-06-17 00:17:07 +0300

    В модуле forms.js реализованы функции, отвечающие за перевод страницы в активное и неактивное состояние.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 import {generateAdsHTML} from './modules/generate-ads-html.js';
+import {disableForms, enableForms} from './modules/form.js';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   generateAdsHTML(3);
+
+  disableForms();
+  // Для проверки работоспособности функции включения форм
+  // временно предусмотрена их активация при клике в произвольном месте
+  document.body.addEventListener('click', enableForms);
 });
