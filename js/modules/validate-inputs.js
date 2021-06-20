9fd3a09f22cd5dfcda3eb9cf467781c928cbc4e2
Andrei <i.andrei.abramov@gmail.com>
2021-06-20 23:59:44 +0300

    Реализовано сопоставление минимальной цены с типом жилья и валиация поля с ценой за ночь.

--- js/modules/validate-inputs.js
+++ js/modules/validate-inputs.js
@@ -3,7 +3,15 @@ const validateInputs = () => {
   const title = adForm.querySelector('#title');
   const timein = adForm.querySelector('#timein');
   const timeout = adForm.querySelector('#timeout');
-
+  const type = adForm.querySelector('#type');
+  const price = adForm.querySelector('#price');
+  const minPrices = {
+    'bungalow': 0,
+    'flat': 1000,
+    'hotel': 3000,
+    'house': 5000,
+    'palace': 10000,
+  };
 
   const validateLength = (field) => {
     const length = field.value.length;
@@ -17,14 +25,37 @@ const validateInputs = () => {
     field.reportValidity();
   };
 
+  const validateNumber = (field, min, max) => {
+    if (field.validity.rangeUnderflow) {
+      field.setCustomValidity(`Минимальное значение для этого поля: ${min}`);
+    } else if (field.validity.rangeOverflow) {
+      field.setCustomValidity(`Максимальное значение для этого поля: ${max}`);
+    } else {
+      field.setCustomValidity('');
+    }
+
+    field.reportValidity();
+  };
+
   const synchronizeSelects = (mainField, dependentField) => {
     dependentField.value = mainField.value;
   };
 
+  const mapPriceWithType = (mapObject) => {
+    price.setAttribute('min', mapObject[type.value]);
+    price.setAttribute('placeholder', mapObject[type.value]);
+  };
+
+  mapPriceWithType(minPrices);
+
+  title.addEventListener('input', () => validateLength(title));
+
   timein.addEventListener('change', () => synchronizeSelects(timein, timeout));
   timeout.addEventListener('change', () => synchronizeSelects(timeout, timein));
 
-  title.addEventListener('input', () => validateLength(title));
+  type.addEventListener('change', () => mapPriceWithType(minPrices));
+  price.addEventListener('input', () => validateNumber(price, minPrices[type.value], price.getAttribute('max')));
+
 
 };
 
