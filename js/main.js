6667128dc4542deeda328b680f6b6786a50640bf
Andrei <i.andrei.abramov@gmail.com>
2021-06-22 23:57:37 +0300

    Реализована загрузка карты с позиционированием в центре Токио. Реализован переход форм в активное состояние после загрузки карты.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-import { generateAdsHTML } from './modules/generate-ads-html.js';
-import { disableForms, enableForms } from './modules/init.js';
+import { disableForms } from './modules/init.js';
 import { replaceSubmitHandler } from './modules/forms.js';
+import  { loadMap } from './modules/map-leaflet.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  generateAdsHTML(3);
-  replaceSubmitHandler('.ad-form');
-
   disableForms();
-  // Для проверки работоспособности функции включения форм
-  // временно предусмотрена их активация при клике в область над картой
-  document.querySelector('.promo').addEventListener('click', enableForms);
+  loadMap();
+  replaceSubmitHandler('.ad-form');
 });
--- js/modules/map-leaflet.js
+++ js/modules/map-leaflet.js
@@ -1,1 +1,18 @@
+import { enableForms } from '../modules/init.js';
 
+const loadMap = () => {
+  const map = L.map('map-canvas')
+    .on('load', enableForms)
+    .setView({
+      lat: 35.6894,
+      lng: 139.692,
+    }, 10);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    },
+  ).addTo(map);
+};
+
+export {loadMap};
